8a1c226dfd87dbfd3c646954cf6a7375546658ae
Bihter Yalçın <76825689+bihter-yalcin@users.noreply.github.com>
2024-08-14 21:53:31 +0300

    Feature-1 Add Business logic for order entity

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.BaseId;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.ProductId;
+
+import java.util.UUID;
+
+public class Product extends BaseEntity<ProductId> {
+    private String name;
+    private Money price;
+
+    public Product(ProductId productId, String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
